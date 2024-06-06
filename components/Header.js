c58d9592b6270a2226d5308ae268443d227fdb59
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-06 19:39:17 -0400

    .

--- components/Header.js
+++ components/Header.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header classe={styles.cabecalho}>
-      <Image height={80}  width={80} src="" />
-      <div classe={styles.icon}>
-        <button classe={styles.carrinho}>Carrinho</button>
-        <button classe={styles.adcProduto}>Adicionar Produto</button>
+    <header>
+      <Image height={80}  width={80} src="https://pngfre.com/wp-content/uploads/cherry-42.png" />
+      <div>
+        <button>Carrinho</button>
+        <button>Adicionar Produto</button>
       </div>
-      <p classe={styles.frase}></p>
+      <p>Para garotinhas!</p>
     </header>
   );
 }
