5e5653bb626a730281fc5c8483d45082bc196f5a
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-06 19:04:39 -0400

    main

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,8 @@ import styles from "./header.module.css";
 import Image from "next/image";
 
 export default function Header(){
-    <header className={styles.cabecalho}></header>
+    <header className={styles.cabecalho}><h1> </h1></header>
+
     return(
     <Image height={80} width={80} src={""}/>
     )};
---
+++ components/Main.js
@@ -0,0 +1,2 @@
+import styles from "./main.module.css";
+import Image from "next/image";
