55af377a5eed1f16a5922dc5bf224e9f23831090
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-06 19:45:36 -0400

    .

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,21 @@
+
 import styles from "./main.module.css";
-import Image from "next/image";
 
 export default async function Main() {
-    
-}
+  const response = await fetch("https://fakestoreapi.com/products");
+  const produtos = await response.json();
 
+  return (
+    <main>
+      {produtos.map((produto) => (
+        <div key={produto.id}>
+          <img src={produto.imagem} />
+          <h3>{produto.titulo}</h3>
+          <p>{produto.descricao}</p>
+          <p>categoria: {produto.categoria}</p>
+          <p>preco: R${produto.preco}</p>
+        </div>
+      ))}
+    </main>
+  );
+}
