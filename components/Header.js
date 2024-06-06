3cb7955b94c24fed8c066500e1f61c0797717fff
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-06 19:36:16 -0400

    .

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,15 @@
 import styles from "./header.module.css";
 import Image from "next/image";
 
-export default function Header(){
-    <header className={styles.cabecalho}><h1> </h1></header>
-
-    return(
-    <Image height={80} width={80} src={""}/>
-    )};
+export default function Header() {
+  return (
+    <header classe={styles.cabecalho}>
+      <Image height={80}  width={80} src="" />
+      <div classe={styles.icon}>
+        <button classe={styles.carrinho}>Carrinho</button>
+        <button classe={styles.adcProduto}>Adicionar Produto</button>
+      </div>
+      <p classe={styles.frase}></p>
+    </header>
+  );
+}
