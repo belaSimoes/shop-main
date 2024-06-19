cb9f71aaed0553b82e418391f607fb60100ccf4a
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-19 15:30:26 -0400

    ajustes

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,15 +1,57 @@
+"use client"
+import { useEffect, useState } from "react";
 import styles from "./main.module.css";
 
-export default async function Main() {
-  const response = await fetch("https://fakestoreapi.com/products", {
-    cache: "no-cache"
-  });
-  const produtos = await response.json();
+export default  function Main() {
 
+  const [listProduct, setProduct] = useState ([]);
+
+  useEffect( ()=> { 
+    const getProduct = async() => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const produtos = await response.json();
+      setProduct(produtos);
+    }
+    getProduct();
+  },[]);
+
+    const orderAz = () => {
+      const listAuxiliar = [...listProduct].sort((a,b) => a.title.localeCompare(b.title) );
+
+      setProduct(listAuxiliar);
+    }
+
+    const orderZa = () => {
+      let listAuxiliar = [...listProduct].sort((a,b) => a.title.localeCompare(b.title) );
+
+      listAuxiliar = listAuxiliar.reverse();
+      setProduct(listAuxiliar);
+    }
+
+    const orderCrescente = () => {
+      const listaOrdem = [...listProduct].sort((a,b) => a.price - b.price);
+      setProduct(listaOrdem);
+    }
+
+    const orderDescrescente = () => {
+      let listaOrdem = [...listProduct].sort((a,b) => a.price - b.price);
+      listaOrdem = listaOrdem.reverse();
+      setProduct(listaOrdem);
+    }
+    
   return (
+    <>
+    <div className={styles.filters}>
+      <div>
+        <button onClick={ orderAz }>Az</button>
+        <button onClick={ orderZa }>Za</button>
+        <button onClick={ orderCrescente }>Ordem Crescente</button>
+        <button onClick={ orderDescrescente }>Ordem Descrescente</button>
+      </div>
+    </div>
     <main>
       <div className={styles.grid}>
-        {produtos.map((produto) => (
+        {listProduct.map((produto) => (
           <div key={produto.id} className={styles.cereja1}>
             <h3>{produto.title}</h3>
             <img src={produto.image} alt={produto.title} className={styles.imagem} />
@@ -21,5 +63,6 @@ export default async function Main() {
         ))}
       </div>
     </main>
+    </>
   );
 }
