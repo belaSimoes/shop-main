778d018d008a629ad67cb147c3a454ae94d62a78
belaSimoes <122486885+belaSimoes@users.noreply.github.com>
2024-06-20 00:53:39 -0400

    centralizando o carregamento

--- src/app/components/Spinner.js
+++ src/app/components/Spinner.js
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import styles from "./spinner.module.css"
 
 export default function Spinner (){
 return (
+    <div className={styles.carregando}>
     <Image width={100} height={100} alt="" src={"/carregando.svg"}/>
+    </div>
 );
 }
